43ba8909286cc82ca16b8d3a470bf82714211703
HidetoOzeki <Ozeki-20.0104@outlook.jp>
2020-11-04 12:53:44 +0900

    Implement Test
    sdl_net implement test compile and run

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <SDL.h>
+#include <SDL_net.h>
 #include <iostream>
 int main(int argc,char* argv[]){
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDLNet_Init();
+  IPaddress ip;
+  //SDLNet_ResolveHost(&ip,"localhost:");
+  SDLNet_Quit();
   std::cout << "Hello Network" << std::endl;
   return 0;
 }
