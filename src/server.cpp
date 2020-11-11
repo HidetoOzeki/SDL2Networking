b07227fe84bd52f6889774ed764412290bedb810
HidetoOzeki <Ozeki-20.0104@outlook.jp>
2020-11-11 14:21:18 +0900

    Server Test

--- src/server.cpp
+++ src/server.cpp
@@ -13,6 +13,18 @@ int main(int argc,char* argv[]){
   TCPsocket server = SDLNet_TCP_Open(&ip);
   TCPsocket client;
 
+  const char* text = "HELLO CLIENT\n";
+  while(1){
+    client = SDLNet_TCP_Accept(server);
+    if(client){
+      SDLNet_TCP_Send(client,text,strlen(text)+1);
+      SDLNet_TCP_Close(client);
+      break;
+    }
+  }
+  SDLNet_TCP_Close(server);
+
   SDLNet_Quit();
   SDL_Quit();
+  return 0;
 }
